382036cee3790f1366d761b7ae19f0fb6471634c
wiltonabreu <wiltonabreuu@gmail.com>
2018-12-13 17:28:39 -0200

    Inicio dos ajustes do site responsivo-mobile

--- js/efeitos.js
+++ js/efeitos.js
@@ -1,36 +1,61 @@
 $(document).ready(function(){
+
 	$("#logotipo").on("mouseover",function(){
+		
 		$("#banner h1").addClass("efeito");
 
-	}).on("mouseout",function(){
+	}).on("mouseout", function(){
+
 		$("#banner h1").removeClass("efeito");
+
 	});
 
 	$("#input-search").on("focus", function(){
+
 		$("li.search").addClass("ativo");
 
 	}).on("blur", function(){
-		$("li.search").removeClass("ativo");
+
+		$("li.search").removeClass("ativo");				
+
 	});
 
 	$(".thumbnails").owlCarousel({
-		loop:true,
-		margin:10,
-		
-		responsive:{
-			0 :{
-				item:1
-			},
-			480:{
-				item:3
-			},
-			768:{
-				item:4
-			},
-			1200:{
-				item:6
-			}
-		}
+ 
+      
+	    responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:1,
+	            nav:true,
+	            loop: true
+	        },
+	        600:{
+	            items:3,
+	            nav:false,
+	            loop: true
+	        },
+	        1000:{
+	            items:4,
+	            nav:true,
+	            loop:true
+	        }
+	    }
+ 
+  	});
+
+	var owl = $(".thumbnails");
+	owl.owlCarousel();
+
+	$('#btn-news-next').click(function() {
+	    owl.trigger('next.owl.carousel');
+	});
+
+	// Go to the previous item
+	$('#btn-news-prev').click(function() {
+	    // With optional speed parameter
+	    // Parameters has to be in square bracket '[]'
+	    owl.trigger('prev.owl.carousel', [300]);
 	});
 
 });
